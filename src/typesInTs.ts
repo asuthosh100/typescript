bc498cacac17dd87867f41e7c96f4c97cb63d0cc
Asu <asuthoshanandaram@gmail.com>
2026-01-13 12:21:59 -0800

    config

---
+++ src/typesInTs.ts
@@ -0,0 +1,16 @@
+let drink = "chain"
+
+let cups = Math.random() > 0.5 ? 10 : '5'
+
+let channelName = "Asu"
+
+
+let chaiFlavor: string = "masala chai"
+
+chaiFlavor = "Gineger tea"
+
+
+let chaiOrder:boolean 
+
+
+// This page covered how TS infers and optionally requires annotations
